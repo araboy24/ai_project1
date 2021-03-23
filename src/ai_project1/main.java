de9e592baf84485b233d786f53c1fa485418629f
Eid Zaben <eid.s.zaben-1@ou.edu>
2021-03-23 17:51:57 +0200

    Basic Iterative Deepening implemented

--- src/ai_project1/main.java
+++ src/ai_project1/main.java
@@ -5,8 +5,15 @@ public class main
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-
+		Node node1 = new Node("A");
+		Node node2 = new Node("B");
+		Node node3 = new Node("C");
+		
+		node1.addNeighbor(node2);
+		node2.addNeighbor(node3);
+		
+		IterativeDeepening itDeep = new IterativeDeepening(node1);
+		itDeep.runDeepeningSearch(node2);
 	}
 
 }
