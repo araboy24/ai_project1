671ae018e59b91106f3c743a67d1b9283870ac00
Eid Zaben <eid.s.zaben-1@ou.edu>
2021-04-13 17:29:53 +0300

    Deleted useless lines of code

--- src/ai_project1/ItDeepCity.java
+++ src/ai_project1/ItDeepCity.java
@@ -10,7 +10,6 @@ public class ItDeepCity
 	private City targetCity;
 	private boolean isTargetFound = false;
 	int depth = 0;
-//	private List<City> citiesToReset = new ArrayList<City>();
 	public ItDeepCity(City targetCity) {
 		this.targetCity = targetCity;
 	}
@@ -23,33 +22,24 @@ public class ItDeepCity
 			LinkedListPractice.resetVisited();
 			depthSearch(startCity, depth);
 			depth++;
-//			resetVisited();
 		}
 	}
 	
-	private void resetVisited() {
-//		for(City c : citiesToReset) {
-//			c.setIsVisited(false);
-//		}
-		
-	}
 	
 	private void depthSearch(City startCity, int depth) {
 		Stack<City> stack = new Stack<>();
 		startCity.setDepthLevel(0);
 		startCity.setIsVisited(true);
 		stack.push(startCity);
-//		citiesToReset.add(startCity);
-//		alreadyCheckedCities.clear();
+
 		
 		while(!stack.isEmpty()) {
 			City actualCity = stack.pop();
-//			alreadyCheckedCities.add(actualCity);
 			System.out.print(actualCity + " ");
 			
 			
 			if(actualCity.getCity().equals(this.targetCity.getCity())) {
-				System.out.println("\nNode has been found");
+				System.out.println("\nCity has been found");
 				System.out.println("Depth reached: "+ depth);
 				this.isTargetFound = true;
 				return;
@@ -59,18 +49,13 @@ public class ItDeepCity
 				continue;
 			}
 			
-//			System.out.println("For loop incoming");
 			for(City city : actualCity.getAdjacentCities()) {
-//				if(!alreadyCheckedCities.contains(city)) {
 				if(!city.getIsVisited()) {
 					city.setDepthLevel(actualCity.getDepthLevel()+1);
 					city.setIsVisited(true);
-//					citiesToReset.add(startCity);
 					stack.push(city);
 				}
-//				}
 			}
-//			System.out.println("For loop Done");
 		}
 	}
 	
